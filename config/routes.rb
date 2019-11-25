57cd6e4c9230df81f1240379da28cf9c6dfb7c14
faithe1937 <faithe1937@gmail.com>
2019-11-25 17:28:32 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create]
 end
+
+#adding :new inside [:index] will create new_post_path
+#adding :create after :new so routing system knows what to do when a POST request is sent to the /posts resource (save after hitting submit)
